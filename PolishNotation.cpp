0bceae9f5d06dd66d5e7801a124d2a63ec8a4b1b
SadBlueYet <88283979+SadBlueYet@users.noreply.github.com>
2022-11-23 23:18:32 +0700

    Update PolishNotation.cpp

--- PolishNotation.cpp
+++ PolishNotation.cpp
@@ -2,163 +2,20 @@
 #include <stack>
 #include <sstream>
 #define _CRT_SECURE_NO_WARNINGS
-//65+65-59*98/65
-/*char polishNotation(char* bufptr);
-int main() {
-    char* buffer = (char*)malloc(1024 * sizeof(char));
-    std::cin >> buffer;
-    polishNotation(buffer);
-	return 0;
-}
-
-char polishNotation(char* bufptr) {
-    char* p = bufptr;
-    int counter = 0;
-    char oper;
-    int couterOutputStr = 0;
-    std::stack <char> stackOper;
-    char* outputString = (char*)malloc(1024 * sizeof(char));
-
-    do{ 
-        
-        if (isdigit(*p)) {
-            outputString[couterOutputStr] = *p;
-            p++;
-            couterOutputStr++;
-            counter++;
-            if(isdigit(*p)) {
-                do {
-                    outputString[couterOutputStr] = *p;
-                    p++;
-                    counter++;
-                    couterOutputStr++;
-                } while (isdigit(*p));
-            }
-            outputString[couterOutputStr] = ' ';
-            couterOutputStr++;
-            counter++;
-            
-            if (counter % 1024 == 0){
-                outputString = (char*)realloc(outputString, 1024 * (counter / 1024 + 1) * sizeof(char));
-                *p = (char)realloc(bufptr, 1024 * (counter / 1024 + 1) * sizeof(char));
-            }
-
-        }
-        if(!isdigit(*p)) {
-            if (stackOper.size() == 0){
-                stackOper.push(*p);
-                p++;
-            }
-            if (*p == '\0' && stackOper.size() != 0) {
-                do { 
-                    outputString[couterOutputStr] = stackOper.top();
-                    stackOper.pop();
-                    outputString[couterOutputStr + 1] = ' ';
-                    couterOutputStr += 2;
-                } while (stackOper.size() != 0);
-                break;
-            }
-            if(stackOper.size() != 0 && !isdigit(*p)) {
-
-                if(*p == '-' || *p == '+' && stackOper.top() == '+' || stackOper.top() == '-') {      
-                    outputString[couterOutputStr] = stackOper.top();
-                    stackOper.pop();
-                    stackOper.push(*p);
-                    outputString[couterOutputStr + 1] = ' ';
-                    couterOutputStr += 2;
-                    counter += 2;
-                    p++;
-                    if (counter % 1024 == 0) {
-                        outputString = (char*)realloc(outputString, 1024 * (counter / 1024 + 1) * sizeof(char));
-                        *p = (char)realloc(bufptr, 1024 * (counter / 1024 + 1) * sizeof(char));
-                    }
-                }
-                if (*p == '*' || *p == '/' && stackOper.top() == '*' || stackOper.top() == '/') {
-                    outputString[couterOutputStr] = stackOper.top();
-                    stackOper.pop();
-                    stackOper.push(*p);
-                    outputString[couterOutputStr + 1] = ' ';
-                    couterOutputStr += 2;
-                    counter += 2;
-                    p++;
-                    if (counter % 1024 == 0) {
-                        outputString = (char*)realloc(outputString, 1024 * (counter / 1024 + 1) * sizeof(char));
-                        *p = (char)realloc(bufptr, 1024 * (counter / 1024 + 1) * sizeof(char));
-                    }
-                }
-                if(!isdigit(*p)) {
-                    stackOper.push(*p);
-                    p++;
-                }
-            }
-  
-        }
-    } while (*p != '\0');
-    outputString[couterOutputStr] = '\0';
-    std::cout << outputString;
-    return *outputString;
-}*/
-/*#include <iostream>
-#include <stack>
-#include <sstream>
-using namespace std;
-int main() {
-    istringstream s("8 9 + 1 7 - *");
-    stack<int> opz;
-    int value;
-    char sign;
-    while (!s.eof())
-    {
-        while (s >> value) {
-            opz.push(value);
-        }
-        if (!s.eof())
-        {
-            s.clear();
-            s.unget();
-            s >> sign;
-            if (opz.size() < 2)
-            {
-                cout << "corrupt oper. order" << endl;
-                return 0;
-            }
-            int right = opz.top();
-            opz.pop();
-            int left = opz.top();
-            opz.pop();
-            switch (sign) {
-            case '+':   opz.push(right + left); break;
-            case '-':   opz.push(right - left); break;
-            case '*':   opz.push(right * left); break;
-            case '/':   opz.push(right / left); break;
-            default:
-                cout << "undefined operatop " << sign << endl;
-                return 0;
-            }
-        }
-    }
-    if (opz.size() != 1)
-    {
-        cout << "stack corrupted" << endl;
-        return 0;
-    }
-    cout << "result value is " << opz.top() << endl;
-    return 0;
-}*/
-void str();
-void exitString(char* bufptr);
+void userInput();
+void isExitString(char* bufptr);
 int main() {
    
-    str();
+    userInput();
     return 0;
 }
-void str() {
+void userInput() {
     char* buffer = (char*)malloc(1024 * sizeof(char));
     std::cin >> buffer;
-    exitString(buffer);      
+    isExitString(buffer);      
 }
 
-void exitString(char* bufptr) {
+void isExitString(char* bufptr) {
     char* p = bufptr;
     std::stack <char> stackOperators;
     char* operand = (char*)malloc(1024 * sizeof(char));
